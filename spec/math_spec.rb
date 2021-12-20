6b1eba2a1f40ea37a3db8853737e607dff6d09c5
harryuan65 <harryuan.65@gmail.com>
2021-12-21 01:16:20 +0800

    feat: format documentation, hw

---
+++ spec/math_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe 'math calculations' do
+  it 'should do proper adding' do
+    expect(101 + 99).to eq(200)
+  end
+  it 'should do proper subtracting' do
+    expect(300 - 193).to eq(107)
+  end
+  it 'should do proper multipling' do
+    expect(64 * 2).to eq(128)
+  end
+  it 'should do proper dividing' do
+    expect(2048 / 2).to eq(1024)
+  end
+end
