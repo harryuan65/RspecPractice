c346ed0403af518440fd7ec6b6f7c2e8694a0a0d
harryuan65 <harryuan.65@gmail.com>
2021-12-24 15:14:35 +0800

    feat: use let to reduce duplication

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Card
-  attr_reader :rank, :suit
+  attr_accessor :rank, :suit
 
   def initialize(rank, suit)
     @rank = rank
@@ -10,14 +10,23 @@ class Card
 end
 
 RSpec.describe 'Card' do
-  before do
-    @card = Card.new('Ace', 'Spades')
-  end
+  let(:card) { Card.new('Ace', 'Spades') }
+
+  # def card
+  #   Card.new('Ace', 'Spades')
+  # end
+
+  # before do
+  #   @card = Card.new('Ace', 'Spades')
+  # end
 
   it 'has a rank' do
-    expect(@card.rank).to eq('Ace')
+    expect(card.rank).to eq('Ace')
   end
+
   it 'has a suit' do
-    expect(@card.suit).to eq('Spades')
+    expect(card.suit).to eq('Spades')
+    card.suit = 'Queen'
+    expect(card.suit).to eq('Queen')
   end
 end
