631a637dee6ce7d6ea32586a3c2213f348a76511
harryuan65 <harryuan.65@gmail.com>
2021-12-24 16:34:31 +0800

    feat: explicit subject#2: with names and implicit access

--- spec/shared/explicit_subject_spec.rb
+++ spec/shared/explicit_subject_spec.rb
@@ -1,12 +1,28 @@
 RSpec.describe Hash do
-  subject do
+  subject(:harry) do
     {
       name: 'harry',
       position: 'Ruby on Rails developer'
     }
   end
 
+  subject(:bob) do
+    {
+      name: 'bob',
+      position: 'Rust Web Developer'
+    }
+  end
+
   it 'has tow k-v pairs' do
+    puts "\e[33mIf having multiple subject and accessing implicitly, the subject will be \"#{subject[:name]}\e[0m\""
     expect(subject.length).to eq(2)
+  end
+
+  it 'harry is harry' do
+    expect(harry[:name]).to eq('harry')
+  end
+
+  it 'bob is bob' do
+    expect(bob[:name]).to eq('bob')
   end
 end
