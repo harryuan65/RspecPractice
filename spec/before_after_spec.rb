93748697a9516d0d43fee34ecdcf8d3db57deb87
harryuan65 <harryuan.65@gmail.com>
2021-12-24 15:45:59 +0800

    feat: before and after CONTEXT hook

--- spec/before_after_spec.rb
+++ spec/before_after_spec.rb
@@ -1,20 +1,37 @@
 RSpec.describe 'before and after hooks' do
+  # But context is just describe, so this hook runs only once for all tests below
+  before(:context) do
+    puts "\e[33m\e[1mBefore context!  \e[0m"
+  end
+
+  after(:context) do
+    puts "\e[31m\e[1mAfter context!  \e[0m"
+  end
+
   before(:example) do
-    puts 'BEFORE!!'
+    puts "\e[36m\e[1m Before example \e[0m"
   end
 
   after(:example) do
-    puts 'AFTER!'
+    puts "\e[31m\e[1m After example \e[0m"
   end
 
-  it 'is just a random example' do
-    puts '20 is just 20'
-    expect(20).to eq(20)
+  context 'when number is 20' do
+    it 'is just a random example' do
+      puts '  20 is just 20'
+      expect(20).to eq(20)
+    end
+    it 'is just a another random example' do
+      puts '  20 is just 20'
+      expect(20).to eq(20)
+    end
   end
 
-  it 'is just a another random example' do
-    puts '20 is just 20'
-    expect(20).to eq(20)
+  context 'when number is 30' do
+    it 'is just a another random example' do
+      puts '  30 is just 30'
+      expect(30).to eq(30)
+    end
   end
 end
 
