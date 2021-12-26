591b07b28e5c93bc9815585a719b8471f9fe6cbb
harryuan65 <harryuan.65@gmail.com>
2021-12-26 13:11:44 +0800

    feat: double#3 method times

--- spec/packages/movie_spec.rb
+++ spec/packages/movie_spec.rb
@@ -33,6 +33,7 @@ class Movie
       puts actor.light_on_fire # only cares about if this guy can light_on_fire/confront/fall_off_ladder
       puts actor.confront
       puts actor.fall_off_ladder
+      puts actor.light_on_fire
     end
   end
 end
@@ -46,9 +47,12 @@ RSpec.describe Movie do
     puts <<-DOC
       \e[33m Movie uses an instance of Actor to shoot.
       \e[36m
+        #1
         The movie can ask actor to do 3 actions.
         Since we are testing the Movie, we don't want the Actor to be an encumbrance.
         Instead we create a test double to simulate an instance of Actor.
+        #2
+        We need to check how many times it is called as well.
       \e[0m
     DOC
   end
@@ -61,12 +65,35 @@ RSpec.describe Movie do
   subject { described_class.new(stuntman) }
 
   describe '#start_shooting' do
-    it 'can ask the actor to do 3 methods' do
+    it 'can ask the actor to do 3 methods #1' do
       # run expectations before the spec is running
       expect(stuntman).to receive(:ready?)
       expect(stuntman).to receive(:light_on_fire)
       expect(stuntman).to receive(:confront)
       expect(stuntman).to receive(:fall_off_ladder)
+      subject.start_shooting
+    end
+
+    it 'can ask the actor to do 3 methods, with light_on_fire exactly twice' do
+      expect(stuntman).to receive(:ready?).once # will stack up if this line is called more than once
+      expect(stuntman).to receive(:light_on_fire).exactly(2).times
+      expect(stuntman).to receive(:confront).once
+      expect(stuntman).to receive(:fall_off_ladder).once
+      subject.start_shooting
+    end
+
+    it 'can ask the actor to do light_on_fire at_most 2 times' do
+      expect(stuntman).to receive(:light_on_fire).at_most(2).times
+      subject.start_shooting
+    end
+
+    it 'can ask the actor to do light_on_fire at_least once' do
+      expect(stuntman).to receive(:light_on_fire).at_least(2)
+      subject.start_shooting
+    end
+
+    it 'can ask the actor to do light_on_fire at_most 2 twice' do
+      expect(stuntman).to receive(:light_on_fire).twice
       subject.start_shooting
     end
   end
