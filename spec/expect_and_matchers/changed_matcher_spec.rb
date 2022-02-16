2bac4072eed88d22bfdc679f36b87ddd5f68a183
harryuan65 <harryuan.65@gmail.com>
2022-02-16 17:22:28 +0800

    feat: add chaining change

--- spec/expect_and_matchers/changed_matcher_spec.rb
+++ spec/expect_and_matchers/changed_matcher_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe 'changed matcher' do
   subject(:arr) { [1, 2, 3] }
-  subject(:harry) { { name: 'harry', role: 'NodeJs Developer' } }
+  subject(:harry) { { name: 'harry', role: 'NodeJs Developer', age: 0, money: 0 } }
 
   it 'checks that a method changes object state' do
     expect { arr.push(4) }.to change { arr.length }.from(3).to(4)
@@ -17,5 +17,18 @@ RSpec.describe 'changed matcher' do
 
   it 'accept negative arguments' do
     expect { arr.pop }.to change { arr.length }.by(-1)
+  end
+
+  it 'can be chained' do
+    expect do
+      harry[:role] = 'Ruby On Rails Developer'
+      harry[:age] = 30
+      harry[:money] += 100
+    end
+      .to(change {  harry[:role] }
+        .from('NodeJs Developer')
+        .to('Ruby On Rails Developer'))
+      .and(change { harry[:age] }.by(30))
+      .and(change { harry[:money] }.by_at_least(80))
   end
 end
